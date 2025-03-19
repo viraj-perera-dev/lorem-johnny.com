7edb50f0889eb972efe4735e73e1b2fe83faefb0
Viraj Perera <viraj.perera.development@gmail.com>
2025-03-19 09:22:13 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 // pages/_app.js
 import Script from 'next/script';
 import '../styles/globals.css'; // Import the global styles
+import { Analytics } from "@vercel/analytics/react"
+
 
 function MyApp({ Component, pageProps }) {
   return (
   <>    
+    <Analytics />
     <Component {...pageProps} />
   </>
 );
